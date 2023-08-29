3eebab8ad6a3c88a17b2d831b2ec7ff257e6be05
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-29 03:54:10 -0600

    feat: Create Models Response

---
+++ src/models/response.go
@@ -0,0 +1,9 @@
+package models
+
+type ResponseProcessReceipts struct {
+	Id string `json:"id" example:"fsdfbshfsfbsdfnsd"`
+}
+
+type ResponseGetPoints struct {
+	Points int `json:"points" example:"56"`
+}
