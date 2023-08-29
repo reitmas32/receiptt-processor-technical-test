1b74288eebe2f302449e45ced458c3e8cfd855eb
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-29 03:53:46 -0600

    feat: Add Model Item and Receipt

---
+++ src/models/receipt.go
@@ -0,0 +1,9 @@
+package models
+
+type Receipts struct {
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Total        string `json:"total"`
+	Items        []Item `json:"items"`
+}
