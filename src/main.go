c0829e588fa02e322a9c168944337d1786859e63
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-29 07:21:05 -0600

    feat: Create main

--- src/main.go
+++ src/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/reitmas32/receiptt-processor-technical-test/src/config"
+	"github.com/reitmas32/receiptt-processor-technical-test/src/routes"
 
+	_ "github.com/reitmas32/receiptt-processor-technical-test/src/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title Receipt Processor
+// @version 1.0
+// @description This is a API by Technical Test by Fetch
+
+// @contact.name Oswaldo Rafael Zamora Ramirez
+// @contact.url https://github.com/reitmas32
+// @contact.email rafa.zamora.rals@gmail.com
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:80
+// @BasePath /
+// @query.collection.format multi
 func main() {
-	fmt.Println("Hello World")
+
+	config.GetRouter().GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	routes.ReceiptsRoutes()
+
+	config.GetRouter().Run(":" + config.PORT)
 }
