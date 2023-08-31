24b1851067796e63dffd145a7a90c72de1e47f3a
Rafael Zamora <42395690+reitmas32@users.noreply.github.com>
2023-08-31 06:20:48 -0600

    Update docs.go

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -185,7 +185,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://receipt-procesor-rafa-zamora.onrender.com",
+	Host:             "receipt-procesor-rafa-zamora.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Receipt Processor",
